84d8a68456800df70deff5edd779509ac06e5c32
Amit <amithel3010@gmail.com>
2026-06-22 14:22:01 +0300

    Add operate function

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
+//calculation vars
+let a;
+let b;
+let operator;
+
+
+function operate(a, b, operator)
+{
+    switch (operator)
+    {
+        case "+":
+            return add(a, b);
+            break;
+
+        case "-":
+            return subtract(a, b);
+            break;
+
+        case "*":
+            return multiply(a, b);
+            break;
+
+        case "/":
+            return divide(a, b);
+            break;
+        
+        default:
+            console.log("unsupported operator used")    
+
+    }
+}
+//basic math functions
 function add(a, b)
 {
     return a + b;
